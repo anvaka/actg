78e9e6caab025a8adf296aae35ac3e1ee8187665
anvaka <anvaka@gmail.com>
2015-11-30 00:11:06 -0800

    Deploy first draft

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   /**
    * Where the data files live
    */
-  endpoint:  'http://localhost:9003/out.count.5',
+  endpoint:  'http://anvaka.github.io/actg/out.count.5',
 
   /**
    * Length of a bounding box for single slice. The larger amount of datapoints
